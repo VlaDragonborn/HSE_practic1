96f536fab25444a4baab8c7ee009343256fe20f9
VlaDragonborn <V.D.SELIFANOV@GMAIL.COM>
2024-09-21 22:14:21 +0300

    Python Operation Update

--- PythonFile.py
+++ PythonFile.py
@@ -1,3 +1,13 @@
 print('Python code')
 number1 = int(input("Введите первое число: "))
 number2 = int(input("Введите второе число: "))
+operation = input("Введите оператор: ")
+if operation == '+':
+    print(number1, '+', number2, '=',  number1 + number2)
+elif operation == '-':
+    print(number1, '-', number2, '=',  number1 - number2)
+elif operation == '*':
+    print(number1, '*', number2, '=',  number1 * number2)
+elif operation == '/':
+    print(number1, '/', number2, '=',  number1 / number2)
+
