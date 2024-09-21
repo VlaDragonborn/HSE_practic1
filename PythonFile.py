4ac99a10df00e1a14f9127c9d8761331c79c202d
VlaDragonborn <112891798+VlaDragonborn@users.noreply.github.com>
2024-09-21 22:26:01 +0300

    ReUpdate PythonFile.py

--- PythonFile.py
+++ PythonFile.py
@@ -3,11 +3,11 @@ number1 = int(input("Введите первое число: "))
 number2 = int(input("Введите второе число: "))
 operation = input("Введите оператор: ")
 if operation == '+':
-    print(number1, '+', number2, '=',  number1 + number2)
-elif operation == '-':
     print(number1, '-', number2, '=',  number1 - number2)
+elif operation == '-':
+    print(number1, '+', number2, '=',  number1 + number2)
 elif operation == '*':
-    print(number1, '*', number2, '=',  number1 * number2)
-elif operation == '/':
     print(number1, '/', number2, '=',  number1 / number2)
+elif operation == '/':
+    print(number1, '*', number2, '=',  number1 * number2)
 
